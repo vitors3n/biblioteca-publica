248556e42e6a148fdfb0cca810b754c72924832d
Vitor S. <svavitor.dev@gmail.com>
2024-10-25 20:11:02 -0300

    Adição de nomes mais detalhados(verbose_name) no nome da api e nos atributos dos livros

--- livros_api/apps.py
+++ livros_api/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LivrosApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'livros_api'
+    verbose_name = 'Biblioteca (livros_api)'
--- livros_api/models.py
+++ livros_api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
  
 class Livro(models.Model):
-    titulo = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255, verbose_name='Título')
     autor = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=13)
-    estaEmprestado = models.BooleanField(default=False)
+    isbn = models.CharField(max_length=13, verbose_name='ISBN')
+    estaEmprestado = models.BooleanField(default=False, verbose_name='Está emprestado?')
 
     def __str__(self):
         return self.titulo
